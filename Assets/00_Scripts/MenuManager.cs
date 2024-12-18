1131b1b84c4d2f9e53d25a4288c020f1a847bb65
Kiyara-Kun <saintthrill17@gmail.com>
2024-12-18 15:21:00 -0300

    Update 18/12/2024 - 15:21
    Arreglo UI, optimizacion de codigo

--- Assets/00_Scripts/MenuManager.cs
+++ Assets/00_Scripts/MenuManager.cs
@@ -3,28 +3,39 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    [SerializeField] private GameObject mainMenuButton;
-    [SerializeField] private GameObject restartButton;
-    [SerializeField] private GameObject playButton;
-    
     public void OnMainMenuButtonClicked()
+    {
+        Debug.Log("Going to main menu, 2s Left...");
+        Invoke(nameof(MainMenu), 2f);
+    }
+    public void OnRestartButtonClicked()
+    {
+        Debug.Log("Restarting Game. 2s Left...");
+        Invoke(nameof(RestartButton), 2f);
+    }
+    public void OnPlayButtonClicked()
+    {
+        Debug.Log("Initializing. 2s Left...");
+        Invoke(nameof(PlayButton), 2f);
+    }
+    private void MainMenu()
     {
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
         SceneManager.LoadScene("Main_Menu");
         GameManager.instance.health = GameManager.instance.maxHealth;
     }
-    public void OnRestartButtonClicked()
+    private void PlayButton()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        SceneManager.LoadScene("Asylum");
+    }
+    private void RestartButton()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
         GameManager.instance.health = GameManager.instance.maxHealth;
-        SceneManager.LoadScene("Stage_01");
-    }
-    public void OnPlayButtonClicked()
-    {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-        SceneManager.LoadScene("Stage_01");
+        SceneManager.LoadScene("Asylum");
     }
 }
